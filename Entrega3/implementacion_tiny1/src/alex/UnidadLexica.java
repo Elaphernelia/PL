bd0dfdebde17fa30eb0c8e97b63921464e52cbd8
David Davó <david@ddavo.me>
2021-05-23 18:08:40 +0200

    Empezando con AST Tiny1

---
+++ Entrega3/implementacion_tiny1/src/alex/UnidadLexica.java
@@ -0,0 +1,30 @@
+package alex;
+
+import java_cup.runtime.Symbol;
+
+public abstract class UnidadLexica extends Symbol {
+	private int fila;
+	private int columna;
+
+	public UnidadLexica(int fila, int columna, int clase, String lexema) {
+		super(clase, lexema);
+		this.fila = fila;
+		this.columna = columna;
+	}
+
+	public int clase() {
+		return sym;
+	}
+
+	public String lexema() {
+		return (String) value;
+	}
+
+	public int fila() {
+		return fila;
+	}
+
+	public int columna() {
+		return columna;
+	}
+}
