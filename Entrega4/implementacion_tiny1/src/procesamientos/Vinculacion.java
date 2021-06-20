82c8b1d5e0a21e8597315fe740055b89579157f3
David Davó <david@ddavo.me>
2021-06-20 19:04:48 +0200

    Paraentregar

--- Entrega4/implementacion_tiny1/src/procesamientos/Vinculacion.java
+++ Entrega4/implementacion_tiny1/src/procesamientos/Vinculacion.java
@@ -142,27 +142,27 @@ public class Vinculacion implements Procesamiento {
 	@Override
 	public void procesa(Var var) {
 		// Construye
+		var.tipo().procesa(this);
+		
 		StringLocalizado id = var.var();
 		if (_t_sim.contieneAct(id)) {
 			errorDec(id);
 		} else {
 			_t_sim.put(id, var);
 		}
-		var.tipo().procesa(this);
 	}
 
 	@Override
 	public void procesa(Type type) {
 		// Construye
 		StringLocalizado id = type.typeName();
+		type.tipo().procesa(this);
 		
 		if (_t_sim.contieneAct(id)) {
 			errorDec(id);
 		} else {
 			_t_sim.put(id, type.tipo());
-		}
-		
-		type.tipo().procesa(this);
+		}		
 	}
 
 	@Override
